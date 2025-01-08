b8d235d447098f03414fb02b15f72c65ee282ab2
TomatoChocolate12 <yashassb1212@gmail.com>
2025-01-08 20:42:57 +0530

    ordering the repo

--- part1/baselineCNN.py
+++ part1/baselineCNN.py
@@ -1,14 +1,12 @@
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset
-import numpy as np
 import glob
-from torchvision import transforms
-from torch.utils.data import DataLoader
-import torch.optim as optim
+from torch.utils.data import Dataset
+
 
 class DigitSumDataset(Dataset):
-    def __init__(self, data_files_pattern='data*.npy', label_files_pattern='lab*.npy', transform=None):
+    def __init__(self, data_files_pattern='../data/data*.npy', label_files_pattern='../data/lab*.npy', transform=None):
         self.data_files = sorted(glob.glob(data_files_pattern))
         self.label_files = sorted(glob.glob(label_files_pattern))
         self.transform = transform
@@ -35,27 +33,31 @@ class DigitSumDataset(Dataset):
         return image, label
 
 class CNNBaseline(nn.Module):
-    def __init__(self):  
+    def __init__(self):
         super(CNNBaseline, self).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),  # 1 channel for grayscale
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
-
-        # Update the fully connected layer to expect 26880 inputs after flattening
+        
+        with torch.no_grad():
+            dummy_input = torch.zeros(1, 1, 40, 168)
+            dummy_output = self.conv_layers(dummy_input)
+            flatten_size = dummy_output.shape[1] * dummy_output.shape[2] * dummy_output.shape[3]
+        
         self.fc_layers = nn.Sequential(
-            nn.Linear(26880, 128),  # Adjusted to the correct flattened size
+            nn.Linear(flatten_size, 128),
             nn.ReLU(),
-            nn.Linear(128, 1)  # Output a single value for the sum
+            nn.Dropout(0.5),
+            nn.Linear(128, 1)
         )
 
     def forward(self, x):
         x = self.conv_layers(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = x.view(x.size(0), -1)
         x = self.fc_layers(x)
         return x
